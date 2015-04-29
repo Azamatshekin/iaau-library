da2422f27afe674d826b4a04e927b4d9e7cd239c
azamatshekin <azamatshekin.kenchimbekov@iaau.edu.kg>
2015-04-29 22:40:05 +0600

    some changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
+    @category = Category.new
   end
 
   # GET /categories/1
