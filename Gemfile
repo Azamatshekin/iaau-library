48123edb3dc07aa3dae5e233045a15c3185e6948
azamatshekin <azamatshekin.kenchimbekov@iaau.edu.kg>
2015-05-13 16:50:16 +0600

    upload picture to profile added

--- Gemfile
+++ Gemfile
@@ -9,6 +9,10 @@ gem 'bcrypt',               '3.1.7'
 # Use sqlite3 as the database for Active Record
 # Use SCSS for stylesheets
 
+#for picture
+gem 'carrierwave',             '0.10.0'
+gem 'mini_magick',             '3.8.0'
+gem 'fog',                     '1.23.0'
 
 gem 'bootstrap-sass', '3.2.0.0'
 
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,8 +1,18 @@
 class Reader < ActiveRecord::Base
   belongs_to :user
   has_many :book_in_uses, :dependent => :destroy
+  mount_uploader :photo, PictureUploader
   validates :name, presence: true, length: {maximum: 50, minimum: 1}
   validates :surname, presence: true, length: {maximum: 50, minimum: 1}
   validates :user_id, presence: true,
             uniqueness: { case_sensitive: false }
+  validate  :picture_size
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if photo.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
